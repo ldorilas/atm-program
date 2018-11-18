438539473a9da68c23611c1638251bdb81908e53
ldorilas <43239931+ldorilas@users.noreply.github.com>
2018-11-18 18:23:55 -0500

    rewritten user file

--- atm.java/src/User.java
+++ atm.java/src/User.java
@@ -1,37 +1,65 @@
-import java.util.Scanner;
-
 public class User {
 	private int pin;
 	private int phone;
 	private String name;
+	private String birthdate;
 	private int ssn;
 	private String address;
 	
-	public User (int pin, int phone, String name, int ssn, String address) {
-		this.pin = pin;
-		this.phone = phone;
-		this.name = name;
-		this.ssn = ssn;
-		this.address = address;
+	public User (int pin, int phone, String name, String birthdate, int ssn, String address) {
+	    this.pin = pin;
+	    this.phone = phone;
+	    this.name = name;
+	    this.birthdate = birthdate;
+	    this.ssn = ssn;
+	    this.address = address;
 	}
 	
 	public int getPin() {
-		return pin;
+	    return pin;
 	}
 	
 	public int getPhone() {
-		return phone;
+	    return phone;
 	}
 	
 	public String getName() {
-		return name;
+	    return name;
+	}
+	
+	public String getBirthdate() {
+	    return birthdate;
 	}
 	
 	public int getSsn() {
-		return ssn;
+	    return ssn;
 	}
 	
 	public String getAddress() {
-		return address;
+	    return address;
+	}
+	
+	public int setPin() {
+	    this.pin = pin;
+	}
+	
+	public int setPhone() {
+	    this.phone = phone;
+	}
+	
+	public String setName() {
+	    this.name = name;
+	}
+	
+	public String setBirthdate() {
+	    this.birthdate = birthdate;
+	}
+	
+	public int setSsn() {
+	    this.ssn = ssn;
+	}
+	
+	public String setAddress() {
+	    this.address = address;
 	}
 }
