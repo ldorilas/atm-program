3f9ecbdfe0d6a082a5c43205f50cfc57bfd6de1b
ldorilas <43239931+ldorilas@users.noreply.github.com>
2018-11-19 08:06:13 -0500

    edit1

--- atm.java/src/ATM.java
+++ atm.java/src/ATM.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 public class ATM extends Account {
-    private Scanner atmMachine;
+    private Scanner input;
     private Account account;
+    
+    Scanner input = new Scanner(System.input);
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        
+    }
     
     public ATM(Account account, float balance, long accountNumber, String accountOwner, double deposit, double withdraw) {
         super(balance, accountNumber, accountOwner, deposit, withdraw);
         this.account = account;
+    }
+    
+    public menu() {
+        
     }
 }
